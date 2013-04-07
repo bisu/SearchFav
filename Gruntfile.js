778bda61cb022c4dd4acdbd838a3ff068afbf648
bisu <michalik.gregory@gmail.com>
2013-04-07 18:36:49 +0200

    added conversion to bookmarklet

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,10 +95,23 @@ module.exports = function(grunt) {
 
   });
 
+  grunt.registerTask("javascript", "add javascript: to convert to bookmarklet", function( filepath, dest ){
+
+    var file;
+
+    var filepath = filepath || "./searchFavourites.min.js";
+    var dest = dest || "./searchFavourites.min.js";
+
+    file = grunt.file.read( filepath );
+    file = "javascript:" + file; 
+    grunt.file.write(dest, file);
+
+  });
+
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask("default", ["build", "timer", "uglify"]);
+  grunt.registerTask("default", ["build", "timer", "uglify", "javascript"]);
   grunt.registerTask("check", ["jshint"]);
 
 };
